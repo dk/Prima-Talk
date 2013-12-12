53240e37abe871644664239129b8a4f2a345720e
David Mertens <dcmertens.perl@gmail.com>
2013-12-12 12:57:36 -0500

    Added is_prima_image to Test::Prima

--- lib/Test/Prima.pm
+++ lib/Test/Prima.pm
@@ -4,13 +4,14 @@ use warnings;
 # A module with testing utilities for Prima systems.
 
 package Test::Prima;
+use Prima;
 
 our $VERSION = "0.01"; # or "0.001_001" for a dev release
 $VERSION = eval $VERSION;
 
 use base 'Test::Builder::Module';
-our @EXPORT = qw( is_prima_color );
-use Scalar::Util qw(looks_like_number);
+our @EXPORT = qw( is_prima_color is_prima_image );
+use Scalar::Util qw(looks_like_number blessed);
 
 sub is_prima_color {
 	my ($got, $expected, $message) = @_;
@@ -19,34 +20,108 @@ sub is_prima_color {
 	
 	# Make sure $got makes sense:
 	if (not defined $got) {
-		my $to_return = $tb->ok(0, $message);
-		$tb->diag('    Got undefined value where expected a defined value');
-		return $to_return;
+		$tb->ok(0, $message);
+		return $tb->diag('    Got undefined value where expected a defined value');
 	}
 	if (not looks_like_number($got)) {
-		my $to_return = $tb->ok(0, $message);
-		$tb->diag("    Got value ($got) that does not look like a Prima color");
-		return $to_return;
+		$tb->ok(0, $message);
+		return $tb->diag("    Got value ($got) that does not look like a Prima color");
 	}
 	
-	# I allow the user to specify a color by name, which makes the diagnostics
-	# a lot cleaner. But we have to make sure that the name exists.
-	my $expected_number = $expected;
-	$expected_number = eval "cl::$expected" if $expected =~ /^[A-Za-z]+$/;
-	$tb->croak("Unkown expected color name $expected") if $@;
-	
-	# OK, we have a numeric color value. Now let's see if they agree
-	my $to_return = $tb->ok($got == $expected_number, $message);
-	return $to_return if $to_return;
+	# Get the color number and pass if they agree.
+	my ($expected_number, $expected_name)
+		= _get_color_number_and_name($expected);
+	$tb->ok($got == $expected_number, $message) and return 1;
 	
 	# If we're here, it means that the comparison failed. Let's give diagnostic
 	# output
-	$tb->diag(	"    Prima color mismatch\n",
-		sprintf('         got: %06x', $got), "\n",
-		sprintf('    expected: %06x%s', $expected_number,
-			($expected_number eq $expected ? '' : " ($expected)")));
+	return $tb->diag(	"    Prima color mismatch\n",
+		_describe_color_mismatch($got, $expected_number, $expected_name)
+	);
+}
+
+sub _get_color_number_and_name {
+	my $expected = shift;
 	
-	return $to_return;
+	# I allow the user to specify a color by name, which makes the diagnostics
+	# a lot cleaner. But we have to make sure that the name exists, and then get
+	# the correct number for it.
+	my $expected_number = $expected;
+	my $expected_name = '';
+	if ($expected =~ /^[A-Za-z]+$/) {
+		# Make sure that the named color exists in the cl package. Note that
+		# 'constant' and 'AUTOLOAD' are incredibly unlikely as intentional
+		# color names, but are possible, so we special-case them.
+		if (not exists $cl::{$expected}
+			or $expected eq 'constant'
+			or $expected eq 'AUTOLOAD'
+		) {
+			Test::Prima->builder->croak("Unkown color name $expected");
+		}
+		# I wish I could get rid of this string eval, but I don't know how to
+		# get at the value of a compile-time constant otherwise. :-(
+		$expected_number = eval "cl::$expected";
+		Test::Prima->builder->croak("Unkown color name $expected") if $@;
+		
+		$expected_name = $expected;
+	}
+	
+	return ($expected_number, $expected_name);
+}
+
+sub _describe_color_mismatch {
+	my ($got, $expected_number, $expected_name) = @_;
+	
+	return '' if $got == $expected_number;
+	return sprintf('         got: %06x', $got), "\n",
+			sprintf('    expected: %06x%s', $expected_number,
+				($expected_name ? " ($expected_name)" : ''));
+}
+
+sub is_prima_image {
+	my ($got, $expected, $message) = @_;
+	$message ||= 'Prima images look the same';
+	my $tb = Test::Prima->builder;
+	
+	# Make sure $expected is a Prima::Image
+	$tb->croak('Expected value is not defined')
+		unless defined $expected;
+	$tb->croak('Expected value is not a Prima::Image')
+		unless blessed($expected) and $expected->isa('Prima::Image');
+	
+	# Sanity-check $got: must be defined, must be a Prima::Image
+	if (not defined $got) {
+		$tb->ok(0, $message);
+		return $tb->diag('    Got undefined value where expected a defined value');
+	}
+	unless (blessed($got) and $got->isa('Prima::Image')) {
+		$tb->ok(0, $message);
+		return $tb->diag("    Got value ($got) is not a Prima::Image");
+	}
+	
+	# Check that the dims match
+	if ($got->width != $expected->width or $got->height != $expected->height) {
+		$tb->ok(0, $message);
+		return $tb->diag("    Image size mismatch\n",
+			"         got: ", $got->width, 'x', $got->height, "\n",
+			"    expected: ", $expected->width, 'x', $expected->height);
+	}
+	
+	# Check each pixel
+	for my $i (0 .. $got->width-1) {
+		for my $j (0 .. $got->height-1) {
+			my $got_px = $got->pixel($i, $j);
+			my $expected_px = $expected->pixel($i, $j);
+			if ($got_px != $expected_px) {
+				$tb->ok(0, $message);
+				return $tb->diag("    Pixel values disagree at ($i, $j)\n",
+					_describe_color_mismatch($got_px, $expected_px));
+			}
+		}
+	}
+	
+	# By process of elimination, the test passes.
+	return $tb->ok(1, $message);
 }
 
 1;
@@ -72,11 +147,14 @@ Test::Prima - a module for testing aspects of the Prima GUI toolkit.
  # Can use color name strings for clearer diagnostic output
  is_prima_color($image->pixel(10, 10), 'Black',
      'Pixels on the diagonal are black');
+ 
+ # Compare two images
+ is_prima_image($got, $expected, 'The two images look alike');
 
 =head1 DESCRIPTION
 
-This module provides a useful function for testing Prima software. At the moment
-it only provides a single test function, but this is likely to grow over time.
+This module provides useful functions for testing Prima software. Presently it
+provides tests for comparing two pixels and comparing two Prima images.
 
 =over
 
@@ -89,6 +167,14 @@ numerical equality. The C<$expected> value can be a Color, or it can be a color
 name, i.e. the string C<'Black'> for the color C<cl::Black>. If the values
 disagree, having the color I<name> makes for a slightly clearer diagnostic
 message.
+
+=item is_prima_image
+
+  is_prima_image($got, $expected, $test_name);
+
+Checks that C<$got> is a L<Prima::Image>, ensures that the dimensions of the
+two images agree, and compares the color value at each coordinate. This will
+throw an exception if C<$expected> is not a Prima image.
 
 =back
 
--- t/01_Test_Prima.t
+++ t/01_Test_Prima.t
@@ -2,11 +2,13 @@
 
 use strict;
 use warnings;
-use Prima qw(Application Label);
+use Prima;
 
-use Test::More tests => 7;
+use Test::More;
 use Test::Builder::Tester;
 use Test::Prima;
+
+#### is_prima_color ####
 
 test_out('ok 1 - Prima colors are equal');
 is_prima_color(cl::Green, cl::Green);
@@ -48,4 +50,104 @@ is_prima_color(cl::Green, 'Blue');
 test_test('Named colors are used if available');
 
 
+#### is_prima_image ####
 
+eval { is_prima_image(undef, undef, 'Undefined value in expected slot croaks') }
+or pass('Undefined value in expected slot croaks');
+like($@, qr/Expected value is not defined/,
+	'Undefined value in expected slot explains why it croaks');
+eval { is_prima_image(undef, 'foo', 'Unblessed value in expected slot croaks') }
+or pass('Unblessed value in expected slot croaks');
+like($@, qr/Expected value is not a Prima::Image/,
+	'Unblessed value in expected slot explains why it croaks');
+eval { is_prima_image(undef, bless({}, 'bar'),
+	'Non-image value in expected slot croaks') }
+or pass('Non-image value in expected slot croaks');
+like($@, qr/Expected value is not a Prima::Image/,
+	'Non-image value in expected slot explains why it croaks');
+
+my $image1 = Prima::Image->new(width => 10, height => 11);
+
+test_out('ok 1 - Prima images look the same');
+is_prima_image($image1, $image1);
+test_test('Comparison of an image with itself passes');
+
+test_out('not ok 1 - Prima images look the same');
+test_fail(+2);
+test_err('/#\s*Got undefined value where expected a defined value/');
+is_prima_image(undef, $image1);
+test_test('Undefined value in $got slot fails with good explanation');
+
+test_out('not ok 1 - Prima images look the same');
+test_fail(+2);
+test_err('/#\s*Got value (.*) is not a Prima::Image/');
+is_prima_image('hello', $image1);
+test_test('Unblessed value in $got slot fails with good explanation');
+
+test_out('not ok 1 - Prima images look the same');
+test_fail(+2);
+test_err('/#\s*Got value (.*) is not a Prima::Image/');
+is_prima_image(bless({}, 'bar'), $image1);
+test_test('Non-image object in $got slot fails with good explanation');
+
+my $image_bad_x = Prima::Image->new(width => 12, height => 11);
+test_out('not ok 1 - Prima images look the same');
+test_fail(+4);
+test_err('/#\s*Image size mismatch/');
+test_err('/#\s*got: 12x11/');
+test_err('/#\s*expected: 10x11/');
+is_prima_image($image_bad_x, $image1);
+test_test('Fails on width mismatch');
+
+my $image_bad_y = Prima::Image->new(width => 10, height => 12);
+test_out('not ok 1 - Prima images look the same');
+test_fail(+4);
+test_err('/#\s*Image size mismatch/');
+test_err('/#\s*got: 10x12/');
+test_err('/#\s*expected: 10x11/');
+is_prima_image($image_bad_y, $image1);
+test_test('Fails on height mismatch');
+
+my $image2 = Prima::Image->new(width => 10, height => 11);
+test_out('ok 1 - Prima images look the same');
+is_prima_image($image2, $image1);
+test_test('Two fresh images with same dimensions look the same');
+
+my ($x, $y) = (int(rand(10)), int(rand(11)));
+$image2->pixel($x, $y, cl::Blue);
+$image1->pixel($x, $y, cl::White);
+test_out('not ok 1 - Prima images look the same');
+test_fail(+4);
+test_err("/#\\s*Pixel values disagree at \\($x, $y\\)/");
+test_err('/#\s*got: .*/');
+test_err('/#\s*expected: .*/');
+is_prima_image($image2, $image1);
+test_test('Pixel mismatches are correctly identified');
+
+# Set $image2 to agree with $image1, and then randomly change various pixel
+# values
+$image2->pixel($x, $y, cl::White);
+for (1 .. 100) {
+	($x, $y) = (int(rand(10)), int(rand(11)));
+	my $color = int(rand(cl::White));
+	$image1->pixel($x, $y, $color);
+	$image2->pixel($x, $y, $color);
+}
+test_out('ok 1 - Prima images look the same');
+is_prima_image($image2, $image1);
+test_test('Two images with random but identical pixel colors pass');
+
+$image1->clear;
+$image2->clear;
+my ($x2, $y2) = (int(rand(10)), int(rand(11)));
+$image1->line($x, $y, $x2, $y2);
+$image2->line($x, $y, $x2, $y2);
+test_out('ok 1 - Prima images look the same');
+is_prima_image($image2, $image1);
+test_test('Two images with identical line draws pass')
+	or diag("Draw line from ($x, $y) to ($x2, $y2)");
+
+# TODO: check that two images with different image formats still agree
+
+
+done_testing;
